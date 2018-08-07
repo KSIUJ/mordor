3f2bec8e1646bd0ef1cde6d0855baf541ced376b
Adam Pardyl <adam@pardyl.com>
2018-08-07 21:03:26 +0200

    Add thymeleaf-layout-dialect

--- src/main/kotlin/pl/edu/uj/ii/ksi/mordor/configuration/MvcConfig.kt
+++ src/main/kotlin/pl/edu/uj/ii/ksi/mordor/configuration/MvcConfig.kt
@@ -3,10 +3,16 @@ package pl.edu.uj.ii.ksi.mordor.configuration
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import nz.net.ultraq.thymeleaf.LayoutDialect
+import org.springframework.context.annotation.Bean
+
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("pl.edu.uj.ii.ksi.mordor.controllers")
 class MvcConfig {
-    //
+    @Bean
+    fun layoutDialect(): LayoutDialect {
+        return LayoutDialect()
+    }
 }
