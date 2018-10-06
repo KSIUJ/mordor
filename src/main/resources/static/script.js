a7924a31bd230d0bcea9bbdaa0774e2350eaac97
Adam Pardyl <adam@pardyl.com>
2018-10-06 23:34:56 +0200

    Update spring to 2.0.5, use bootstrap css

--- src/main/resources/static/script.js
+++
@@ -1,8 +0,0 @@
-
-for (var elem of document.querySelectorAll(".clickable-row")) {
-    (function(elem) {
-        elem.addEventListener('click', function() {
-            elem.querySelector('a').click();
-        });
-    })(elem);
-}
