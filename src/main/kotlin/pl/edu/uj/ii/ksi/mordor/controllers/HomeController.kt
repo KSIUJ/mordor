083761dc885a98946d34dff458497d189c5020ad
Adam Pardyl <adam@pardyl.com>
2018-11-10 17:33:45 +0100

    Landing page and footer

--- src/main/kotlin/pl/edu/uj/ii/ksi/mordor/controllers/HomeController.kt
+++ src/main/kotlin/pl/edu/uj/ii/ksi/mordor/controllers/HomeController.kt
@@ -1,12 +1,17 @@
 package pl.edu.uj.ii.ksi.mordor.controllers
 
+import java.security.Principal
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class HomeController {
     @GetMapping(value = ["/"])
-    fun homePage(): String {
-        return "home"
+    fun homePage(principal: Principal?): String {
+        return if (principal != null) {
+            "redirect:/file/"
+        } else {
+            "home"
+        }
     }
 }
