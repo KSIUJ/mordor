affeac4071e58396080d661d23613fcf5caf0bf8
DominikWolek <dominik.artur.wolek@gmail.com>
2020-04-21 23:44:48 +0200

    Revert "Add status to FileEntry"
    This reverts commit affa49e25c00627fa229e36eb27345b4d1198cc9.

--- src/main/kotlin/pl/edu/uj/ii/ksi/mordor/persistence/entities/FileEntry.kt
+++ src/main/kotlin/pl/edu/uj/ii/ksi/mordor/persistence/entities/FileEntry.kt
@@ -2,8 +2,6 @@ package pl.edu.uj.ii.ksi.mordor.persistence.entities
 
 import javax.persistence.CascadeType
 import javax.persistence.Entity
-import javax.persistence.EnumType
-import javax.persistence.Enumerated
 import javax.persistence.Id
 import javax.persistence.ManyToOne
 
@@ -13,8 +11,5 @@ data class FileEntry(
     var path: String,
 
     @ManyToOne(optional = false, cascade = [CascadeType.DETACH])
-    var metadata: FileMetadata,
-
-    @Enumerated(EnumType.STRING)
-    var status: FileEntryStatus
+    var metadata: FileMetadata
 )
--- src/main/kotlin/pl/edu/uj/ii/ksi/mordor/persistence/entities/FileEntryStatus.kt
+++
@@ -1,5 +0,0 @@
-package pl.edu.uj.ii.ksi.mordor.persistence.entities
-
-enum class FileEntryStatus {
-    PENDING, APPROVED, REJECTED
-}
