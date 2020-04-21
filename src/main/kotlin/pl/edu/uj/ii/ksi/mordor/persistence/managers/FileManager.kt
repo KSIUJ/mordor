0ef6b291d66ba38707fd438b8cd674efda02f460
DominikWolek <dominik.artur.wolek@gmail.com>
2020-04-21 18:45:09 +0200

    Delete unecessary functions from FileManager

--- src/main/kotlin/pl/edu/uj/ii/ksi/mordor/persistence/managers/FileManager.kt
+++ src/main/kotlin/pl/edu/uj/ii/ksi/mordor/persistence/managers/FileManager.kt
@@ -2,15 +2,11 @@ package pl.edu.uj.ii.ksi.mordor.persistence.managers
 
 import java.io.File
 import pl.edu.uj.ii.ksi.mordor.persistence.entities.FileEntry
-import pl.edu.uj.ii.ksi.mordor.persistence.entities.FileMetadata
 
 interface FileManager {
     fun addFile(file: File, path: String)
 
     fun createDirectory(path: String)
-
-    fun getFile(path: String): File
-    fun getFile(metadata: FileMetadata): File
 
     fun getFileEntry(path: String): FileEntry?
 
