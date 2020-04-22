204bd505ac1cbc0d43ffb59375dec23b814e331c
DominikWolek <dominik.artur.wolek@gmail.com>
2020-04-22 17:06:38 +0200

    Finish FileEntryService

--- src/main/kotlin/pl/edu/uj/ii/ksi/mordor/services/FileEntryService.kt
+++ src/main/kotlin/pl/edu/uj/ii/ksi/mordor/services/FileEntryService.kt
@@ -1,38 +1,24 @@
 package pl.edu.uj.ii.ksi.mordor.services
 
 import java.io.File
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import pl.edu.uj.ii.ksi.mordor.persistence.entities.FileEntry
 import pl.edu.uj.ii.ksi.mordor.persistence.repositories.FileEntryRepository
 import pl.edu.uj.ii.ksi.mordor.services.repository.RepositoryService
-import pl.edu.uj.ii.ksi.mordor.services.upload.session.FileUploadSessionService
 
 @Service
 class FileEntryService(
-    private var fileUploadSessionService: FileUploadSessionService,
     private val repositoryService: RepositoryService,
-    @Value("\${mordor.pending_sessions_path}") private val pendingSessionPath: String,
     private val entryRepository: FileEntryRepository
 ) {
     fun fileExists(entry: FileEntry): Boolean {
-        val fullPath = getFullPath(entry)
-        return repositoryService.getEntity(fullPath) != null
+        return repositoryService.getEntity(entry.path) != null
     }
 
     fun checkFile(entry: FileEntry) {
         if (!fileExists(entry)) {
             entryRepository.delete(entry)
         }
-    }
-
-    private fun getFullPath(entry: FileEntry): String {
-        return getPrefix(entry) + entry.path
-    }
-
-    private fun getPrefix(entry: FileEntry): String {
-        // TODO
-        return ""
     }
 
     fun isFileValidEntryCandidate(file: File): Boolean {
