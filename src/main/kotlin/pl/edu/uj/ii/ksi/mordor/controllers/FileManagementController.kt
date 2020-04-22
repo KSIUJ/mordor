f70d0cf18067810955c5f3b09b2f10b3ab068735
Jakub Kiermasz <kiermasz.k@gmail.com>
2020-04-22 16:35:19 +0200

    Session review list

--- src/main/kotlin/pl/edu/uj/ii/ksi/mordor/controllers/FileManagementController.kt
+++ src/main/kotlin/pl/edu/uj/ii/ksi/mordor/controllers/FileManagementController.kt
@@ -12,8 +12,14 @@ import pl.edu.uj.ii.ksi.mordor.forms.FileUploadForm
 
 @Controller
 class FileManagementController {
+    data class SessionEntry(
+        val userId: Long,
+        val userName: String,
+        val sessionId: String
+    )
+
     @GetMapping("/management/upload/")
-    fun uploadPage(): ModelAndView {
+    fun fileUploadPage(): ModelAndView {
         return ModelAndView("management/upload", "form", FileUploadForm())
     }
 
@@ -26,5 +32,16 @@ class FileManagementController {
             return ModelAndView("management/upload", HttpStatus.BAD_REQUEST)
         }
         return ModelAndView("home")
+    }
+
+    @GetMapping("/review/")
+    fun sessionReviewList(): ModelAndView {
+        val sessions = arrayOf<SessionEntry>()
+
+        // TODO: - sort by timestamp
+        val sortedSessions = sessions.sortedBy { it.userName }
+        return ModelAndView("review/list", mapOf(
+                "sessions" to sortedSessions
+        ))
     }
 }
