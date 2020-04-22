5e9daedc0b64aa983a2e5f39236f10cf0470f9b8
DominikWolek <dominik.artur.wolek@gmail.com>
2020-04-22 18:57:42 +0200

    Add getAllFiles to FileUploadSession

--- src/main/kotlin/pl/edu/uj/ii/ksi/mordor/services/upload/session/FileUploadSessionService.kt
+++ src/main/kotlin/pl/edu/uj/ii/ksi/mordor/services/upload/session/FileUploadSessionService.kt
@@ -1,13 +1,17 @@
 package pl.edu.uj.ii.ksi.mordor.services.upload.session
 
+import org.apache.commons.io.monitor.FileEntry
 import org.springframework.stereotype.Service
 
 @Service
 class FileUploadSessionService {
-    // TODO: Implementation
     fun accept(uploadSession: FileUploadSession) {}
 
     fun reject(uploadSession: FileUploadSession) {}
+
+    fun getAllFiles(uploadSession: FileUploadSession): List<FileEntry> {
+        return ArrayList()
+    }
 
     fun getAllSessions(): List<FileUploadSession> {
         return ArrayList()
